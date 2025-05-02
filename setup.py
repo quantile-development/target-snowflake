3b40294d683f49a983b0ce145671a1db4d8224cd
BernardWez <bernard.wezeman@quantile.nl>
2025-05-02 17:22:57 +0200

    Remove conflicting dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         'target-redshift==0.2.4',
         'botocore<1.13.0,>=1.12.253',
         'snowflake-connector-python==2.2.5',
-        'cryptography>=36.0.0'  # Added for private key support
     ],
     setup_requires=[
         "pytest-runner"
